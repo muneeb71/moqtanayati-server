363a770a123269ca63a4f10e518640a3b1a29bc4
Abdullah-Farhan <102550496+Abdullah-Farhan@users.noreply.github.com>
2025-07-02 15:41:49 +0500

    feat/ sub categories table

--- src/modules/auth/services/auth.service.js
+++ src/modules/auth/services/auth.service.js
@@ -3,11 +3,6 @@ const twilio = require('twilio');
 const nodemailer = require('nodemailer');
 const bcrypt = require('bcryptjs');
 const generateOtp = require('../../../utils/otp');
-const { PrismaClient } = require("@prisma/client");
-const twilio = require("twilio");
-const nodemailer = require("nodemailer");
-const bcrypt = require("bcryptjs");
-const generateOtp = require("../../../utils/otp");
 
 const prisma = new PrismaClient();
 
